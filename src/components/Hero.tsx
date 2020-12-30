bcf59b620ce5c3193d4dfd01afdf20a9381ad22c
Beatrix Woo <beatrixwho@gmail.com>
2020-12-29 22:12:48 -0800

    base created

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import Image from 'next/image';
 
 export default function Hero({ src }) {
     return (
-    <div className="flex">
-        <Image src={src} width="792" height="373" />
+    <div className="flex space-x-0 p-2 max-w-screen-md" >
+        <div className="w-3/5">
+        <div className="w-3/4 h-full p-6">
+            <p className="font-sans text-lg md:text-4xl font-extrabold">
+                Your Custom Star Map</p>
+            <p className="pt-5 font-light">
+                Create a 18"x24" star map that shows the stars exactly as they were on a specific date and location with a personalized message!
+            </p>
+
+            <div className="pt-10">
+            <button className="py-3 px-7 font-sans shadow-yellow rounded text-white hover:text-yellow-100 bg-gradient-to-b from-yellow-400 to-yellow-500 shadow-2xl hover:bg-yellow-500">
+                Create Your Star Map
+            </button>
+            </div>
+            </div>
+        </div>
+        <div className="flex-auto bg-black p-2 w-1/4 relative" >
+        <img alt="image" src={src} className="flex-none object-cover bg-black" />
+        </div>
     </div>
 
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/components/**/*.{ts,tsx,js,jsx}', './src/pages/**/*.{ts,tsx,js,jsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        yellow: '0 15px 15px 10px rgba(254, 243, 199, var(--tw-text-opacity))',
+      },
+    },
   },
   variants: {
     extend: {},

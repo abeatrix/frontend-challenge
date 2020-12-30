8b5813dceb752da52c6276282d37f62de37e9a60
Beatrix Woo <beatrixwho@gmail.com>
2020-12-30 00:41:28 -0800

    install axios and use it to retrieve data from API to work with hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,28 +1,24 @@
-import React from "react";
-
-export default function Hero({ src }) {
+export default function Hero({ data }) {
   return (
     <div className="flex space-x-0 p-2 max-w-screen-md">
       <div className="grid grid-cols-3">
-        <div className="col-span-2 h-full p-3">
+        <div className="col-span-2 h-full p-5 md:pr-44">
           <p className="font-sans text-base md:text-4xl font-extrabold">
-            Your Custom Star Map
+            {data.header}
           </p>
           <p className="pt-1 md:pt-5 text-sm md:text-lg font-light">
-            Create a 18"x24" star map that shows the stars exactly as they were
-            on a specific date and location with a personalized message!
+            {data.content}
           </p>
 
           <div className="mt-5 md:mt-10">
             <button className="py-3 md:px-7 px-3 font-sans md:text-base text-xs shadow-yellow rounded text-white hover:text-yellow-100 bg-gradient-to-b from-yellow-400 to-yellow-500 shadow-2xl hover:bg-yellow-500">
-              Create Your Star Map
+              {data.cta}
             </button>
           </div>
         </div>
-      </div>
-
-      <div className="flex-none w-1/3 relative p-2 bg-black">
-        <img alt="image" src={src} className="object-cover" />
+        <div className="flex p-2 bg-black">
+          <img alt="image" src={data.src} className="object-cover w-full" />
+        </div>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
 import Hero from "../components/Hero";
 
 export default function Home() {
+
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        axios.post("http://localhost:3000/api/cms").then(response => setData(response.data));
+    }, [])
+
     return (
         <div className="container flex items-center mx-auto justify-center">
-            <Hero src="/images/hero.png" />
+            <Hero data={data} />
         </div>
     )
 }

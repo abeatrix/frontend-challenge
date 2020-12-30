a4789b258b6bc8193c8acfa7be27e2e98a38bf7e
Beatrix Woo <beatrixwho@gmail.com>
2020-12-30 01:04:10 -0800

    add typescript functions and loading state

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,35 @@
-export default function Hero({ data }) {
+interface dataProps {
+  header: string;
+  content: string;
+  cta: string;
+  src: string;
+}
+
+const Hero: React.FC<dataProps> = ({ header, content, cta, src }) => {
+
   return (
     <div className="flex space-x-0 p-2 max-w-screen-md">
       <div className="grid grid-cols-3">
         <div className="col-span-2 h-full p-5 md:pr-44">
           <p className="font-sans text-base md:text-4xl font-extrabold">
-            {data.header}
+            {header}
           </p>
           <p className="pt-1 md:pt-5 text-sm md:text-lg font-light">
-            {data.content}
+            {content}
           </p>
 
           <div className="mt-5 md:mt-10">
             <button className="py-3 md:px-7 px-3 font-sans md:text-base text-xs shadow-yellow rounded text-white hover:text-yellow-100 bg-gradient-to-b from-yellow-400 to-yellow-500 shadow-2xl hover:bg-yellow-500">
-              {data.cta}
+              {cta}
             </button>
           </div>
         </div>
         <div className="flex p-2 bg-black">
-          <img alt="image" src={data.src} className="object-cover w-full" />
+          <img alt="image" src={src} className="object-cover w-full" />
         </div>
       </div>
     </div>
   );
 }
+
+export default Hero
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,21 @@ import axios from 'axios'
 import Hero from "../components/Hero";
 
 export default function Home() {
-
+    const [fetched, setFetched] = useState(false)
     const [data, setData] = useState("");
 
     useEffect(() => {
-        axios.post("http://localhost:3000/api/cms").then(response => setData(response.data));
+        axios.post("http://localhost:3000/api/cms")
+        .then(response => {setData(response.data), setFetched(true)})
+        .catch((error) => {
+            console.error(error);
+            return Promise.reject(error);
+        });
     }, [])
 
     return (
         <div className="container flex items-center mx-auto justify-center">
-            <Hero data={data} />
+            { fetched? <Hero header={data.header} content={data.content} cta={data.cta} src={data.src} /> : <p>loading...</p>}
         </div>
     )
 }

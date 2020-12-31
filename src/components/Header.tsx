a71e14a406a0948ad58d8bdb268bbdcfa37a6b97
Beatrix Woo <beatrixwho@gmail.com>
2020-12-30 23:33:48 -0800

    add bg clipped text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex opacity-80 bg-header-bg bg-cover w-full bg-no-repeat h-auto p-60 items-center justify-center ">
+    <div className="flex-col opacity-80 bg-header-bg bg-cover w-full bg-no-repeat h-auto p-60 items-center justify-center text-center">
       {/* <video className="object-cover" autoPlay loop muted src="/images/video.mp4" /> */}
-      <div className="text-5xl font-extrabold ...">
+      <div className="text-5xl font-extrabold transform hover:scale-110 motion-reduce:transform-none">
         <span className="bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-blue-500">
-          Find Your Stars
+          We Found Your Stars
         </span>
       </div>
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,31 +8,37 @@ interface dataProps {
 const Hero: React.FC<dataProps> = ({ header, content, cta, src }) => {
   return (
     <div className=" relative mh-1/3 flex items-center justify-center">
-      <video className="object-cover w-full h-full bg-black z-0" autoPlay loop muted src="/images/video.mp4" />
+      <video
+        className="object-cover w-full h-full bg-black z-0"
+        autoPlay
+        loop
+        muted
+        src="/images/video.mp4"
+      />
       <div className="z-30 mw-full absolute p2 flex-col items-center">
+        <div className="md:flex max-w-md mx-auto md:max-w-4xl p-2">
+          <div className="flex-initial h-full p-5 md:pr-44 sm:text-left text-center">
+            <p className="font-sans text-xl sm:text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-yellow-300 to-white">
+              {header}
+            </p>
+            <p className="pt-2 md:pt-5 sm:pr-20 text-sm sm:text-lg font-light text-white">
+              {content}
+            </p>
 
-      <div className="md:flex max-w-md mx-auto md:max-w-4xl p-2">
-      <div className="flex-initial h-full p-5 md:pr-44 sm:text-left text-center">
-        <p className="font-sans text-xl sm:text-4xl font-extrabold text-white">{header}</p>
-        <p className="pt-2 md:pt-5 sm:pr-20 text-sm sm:text-lg font-light text-white">
-          {content}
-        </p>
-
-        <div className="mt-2 md:mt-20">
-          <button className="py-3 md:px-9 px-3 font-sans md:text-base text-xs md:shadow-yellow rounded text-white hover:text-yellow-100 bg-gradient-to-b from-yellow-400 to-yellow-500 shadow-2xl hover:bg-yellow-500">
-            {cta}
-          </button>
+            <div className="mt-2 md:mt-20">
+              <button className="py-3 md:px-9 px-3 font-sans md:text-base text-xs rounded text-white bg-gradient-to-b from-yellow-400 to-yellow-200 shadow-2xl transform hover:scale-110 motion-reduce:transform-none">
+                {cta}
+              </button>
+            </div>
+          </div>
+          <div className="md:flex-auto w-full h-full">
+            <img
+              alt="image"
+              src={src}
+              className="border-8 border-black object-cover w-full transform hover:scale-110 motion-reduce:transform-none"
+            />
+          </div>
         </div>
-      </div>
-      <div className="md:flex-auto  w-full h-full">
-        <img
-          alt="image"
-          src={src}
-          className="border-8 border-black object-cover w-full"
-        />
-      </div>
-    </div>
-
       </div>
     </div>
   );
